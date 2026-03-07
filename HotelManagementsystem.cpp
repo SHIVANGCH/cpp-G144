04f01fe692255b1db9f41f8769f735d63592bf7f
RishankSherwal <rishanksherwal@gmail.com>
2026-03-07 16:05:10 +0530

    update

---
+++ HotelManagementsystem.cpp
@@ -0,0 +1,51 @@
+#include<iostream>
+using namespace std;
+class Person{
+protected:
+    string name;
+    int age;
+    string phone;
+public:
+  Person(){
+        name= ;
+        age=0;
+        phone="";
+}
+
+    void inputPerson(){
+        cout<<"Enter Name: ";
+        cin>>name;
+
+        cout<<"Enter Age: ";
+        cin>>age;
+
+        cout<<"Enter Phone Number: ";
+        cin>>phone;
+}
+
+    void displayPerson(){
+        cout<<"Name : "<<name<<endl;
+        cout<<"Age : "<<age<<endl;
+        cout<<"Phone : "<<phone<<endl;
+}
+};
+class Customer : public Person
+{
+
+private:
+
+    int customerID;
+
+public:
+
+    Customer()
+    {
+        customerID=0;
+    }
+
+    void inputCustomer()
+    {
+        inputPerson();
+
+        cout<<"Enter Customer ID : ";
+        cin>>
