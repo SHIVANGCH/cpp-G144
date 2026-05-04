f357719b71fae85cb9f6b9ca0720c717605b8e11
SHIVANG CHATURVEDI <shivangchaturvedi20@gmail.com>
2026-05-04 12:14:26 +0530

    Update HotelManagementsystem.cpp

--- HotelManagementsystem.cpp
+++ HotelManagementsystem.cpp
@@ -126,3 +126,105 @@ public:
         cout<<"Room Number : "<<roomNumber<<endl;
     }
 };
+/* ---------------------------
+   STANDARD ROOM
+---------------------------*/
+
+class StandardRoom : public Room
+{
+public:
+    int getPrice()
+    {
+        return 2000;
+    }
+
+    void displayRoom()
+    {
+        cout<<"Room Number : "<<roomNumber<<endl;
+        cout<<"Room Type : Standard"<<endl;
+        cout<<"Price : 2000 per night"<<endl;
+    }
+};
+
+/* ---------------------------
+   DELUXE ROOM
+---------------------------*/
+
+class DeluxeRoom : public Room
+{
+public:
+    int getPrice()
+    {
+        return 4000;
+    }
+
+    void displayRoom()
+    {
+        cout<<"Room Number : "<<roomNumber<<endl;
+        cout<<"Room Type : Deluxe"<<endl;
+        cout<<"Price : 4000 per night"<<endl;
+    }
+};
+
+/* ---------------------------
+   BOOKING CLASS
+---------------------------*/
+
+class Booking
+{
+private:
+    Customer customer;
+    Room* room;
+    int days;
+    int bill;
+
+public:
+    Booking()
+    {
+        days=0;
+        bill=0;
+        room=NULL;
+    }
+
+    void createBooking(Room* r)
+    {
+        try
+        {
+            if(r->isBooked())
+            {
+                throw "Room already booked!";
+            }
+
+            room=r;
+
+            cout<<"\nEnter Customer Details\n";
+            customer.inputCustomer();
+
+            cout<<"Enter number of days : ";
+            cin>>days;
+
+            r->bookRoom();
+
+            bill = r->getPrice() * days;
+
+            cout<<"Booking successful!\n";
+        }
+        catch(const char* msg)
+        {
+            cout<<"Error : "<<msg<<endl;
+        }
+    }
+
+    void displayBooking()
+    {
+        cout<<"\n------ BOOKING DETAILS ------\n";
+
+        customer.displayCustomer();
+
+        cout<<"Room Number : "<<room->getRoomNumber()<<endl;
+
+        cout<<"Days Stayed : "<<days<<endl;
+
+        cout<<"Total Bill : "<<bill<<endl;
+    }
+};
